61100ecb52b226972de5c8acd3ef0685a6973446
odelolajosh <oladapoodelola@gmail.com>
2023-01-19 23:48:28 +0100

    fix: handle necessary err

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,7 +3,7 @@
 import sys
 
 
-statuses = ["200", "301", "400", "401", "403", "404", "405", "500"]
+statuses = ("200", "301", "400", "401", "403", "404", "405", "500")
 
 
 def print_stats(total_size, status_codes):
@@ -17,20 +17,24 @@ total_size = 0
 status_codes = dict()
 count = 0
 
-try:
-    for line in sys.stdin:
-        fields = line.split()[::-1]     # reverse split log
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            count += 1
+            try:
+                fields = line.split()[::-1]     # reverse split log
 
-        if count == 10:
-            print_stats(total_size, status_codes)
-            count = 0
-            continue
+                if count == 10:
+                    print_stats(total_size, status_codes)
+                    count = 0
+                    continue
 
-        total_size += int(fields[0])
-        status = fields[1]
-        if status in statuses:
-            status_codes[status] = status_codes.get(status, 0) + 1
-        count += 1
-except KeyboardInterrupt:
-    print_stats(total_size, status_codes)
-    raise
+                total_size += int(fields[0])
+                status = fields[1]
+                if status in statuses:
+                    status_codes[status] = status_codes.get(status, 0) + 1
+            except:
+                pass
+    except KeyboardInterrupt as err:
+        print_stats(total_size, status_codes)
+        raise err
