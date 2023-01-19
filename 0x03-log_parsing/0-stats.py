77b0b62e55a9a4e91f28a67d737a4f5fd7d6464f
odelolajosh <oladapoodelola@gmail.com>
2023-01-20 00:44:26 +0100

    fix: fixes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
 """ 0. Log parsing """
 import sys
-import re
-from collections import defaultdict
 
 
-def print_stats(total_size, status_codes):
-    """ Prints a log stats. """
-    print("File size: {}".format(total_size_)
-    for status, count in sorted(status_codes.items()):
-        print("{}: {}".format(status, count)
+def print_stats(size, obj):
+    """ Prints log stats """
+    print("File size: {}".format(size))
+    for key, val in sorted(obj.items()):
+        print("{}: {}".format(key, val))
 
 
-exp = r"^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}\s-\s\[.*\]\s\".*\"\s(\d{3})\s(\d+)\n$"
+codes = ("200", "301", "400", "401", "403", "404", "405", "500")
+status_obj = dict()
+acc_size = 0
 
-
-if __name__ == "__main__":
-    total_size = 0
-    status_codes = defaultdict(lambda: 0)
+try:
     count = 0
-    statuses = ("200", "301", "400", "401", "403", "404", "405", "500")
+    for line in sys.stdin:
+        count += 1
+        fields = line.split()[::-1]    # reverse split
+        try:
+            acc_size += int(fields[0])
+            code = fields[1]
 
-    try:
-        for line in sys.stdin:
-            count += 1
-
-            print(line, end='')
-            match = re.fullmatch(exp, line)   # match standard log expression
-            if not match:
-                continue
-
-            code, file_size = match.groups()
-
-            total_size += int(file_size)
-            if code in statuses:
-                status_codes[code] += 1
+            if code in codes:
+                status_obj[code] = status_obj.get(code, 0) + 1
 
             if count == 10:
-                print_stats(total_size, status_codes)
+                print_stats(acc_size, status_obj)
                 count = 0
-    except KeyboardInterrupt as err:
-        print_stats(total_size, status_codes)
-        raise err
+        except Exception:
+            pass
+except KeyboardInterrupt:
+    print_stats(acc_size, status_obj)
+    raise
