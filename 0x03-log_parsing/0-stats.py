cbd0a71c78b7b0e6b21f1cf881b07c0eb8ead2f4
odelolajosh <oladapoodelola@gmail.com>
2023-01-19 23:20:05 +0100

    fix: only import sys

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """ 0. Log parsing """
 import sys
-from collections import defaultdict
-from typing import Dict
 
 
 statuses = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
 
-def print_stats(total_size: int, status_codes: Dict[str, int]):
+def print_stats(total_size, status_codes):
     """ Prints a log stats. """
     print(f"File size: {total_size}")
     for status, count in sorted(status_codes.items()):
@@ -17,7 +15,7 @@ def print_stats(total_size: int, status_codes: Dict[str, int]):
 
 if __name__ == "__main__":
     total_size = 0
-    status_codes = defaultdict(lambda: 0)
+    status_codes = dict()
     count = 0
 
     try:
@@ -32,7 +30,7 @@ if __name__ == "__main__":
             total_size += int(fields[0])
             status = fields[1]
             if status in statuses:
-                status_codes[status] += 1
+                status_codes[status] = status_codes.get(status, 0) + 1
             count += 1
     finally:
         print_stats(total_size, status_codes)
