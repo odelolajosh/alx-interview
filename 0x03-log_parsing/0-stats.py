1c52e15eed69cd872a155529cc7243a6348e9086
odelolajosh <oladapoodelola@gmail.com>
2023-01-20 00:46:32 +0100

    fix: fixes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,11 +7,12 @@ def print_stats(size, obj):
     """ Prints log stats """
     print("File size: {}".format(size))
     for key, val in sorted(obj.items()):
-        print("{}: {}".format(key, val))
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
 codes = ("200", "301", "400", "401", "403", "404", "405", "500")
-status_obj = dict()
+status_obj = {code: 0 for code in codes}
 acc_size = 0
 
 try:
@@ -24,7 +25,7 @@ try:
             code = fields[1]
 
             if code in codes:
-                status_obj[code] = status_obj.get(code, 0) + 1
+                status_obj[code] += 1
 
             if count == 10:
                 print_stats(acc_size, status_obj)
