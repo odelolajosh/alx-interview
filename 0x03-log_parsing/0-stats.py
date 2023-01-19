f21d7ff6094c4ea6e503457a23da3221a36c90d0
odelolajosh <oladapoodelola@gmail.com>
2023-01-20 00:43:28 +0100

    fix: fixes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
 """ 0. Log parsing """
 import sys
+import re
+from collections import defaultdict
 
 
-def print_stats(size, obj):
-    """ Prints log stats """
-    print("File size: {}".format(size))
-    for key, val in sorted(obj.items()):
-        print('{}: {}'.format(key, val))
+def print_stats(total_size, status_codes):
+    """ Prints a log stats. """
+    print("File size: {}".format(total_size_)
+    for status, count in sorted(status_codes.items()):
+        print("{}: {}".format(status, count)
 
 
-codes = ("200", "301", "400", "401", "403", "404", "405", "500")
-status_obj = dict()
-acc_size = 0
+exp = r"^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}\s-\s\[.*\]\s\".*\"\s(\d{3})\s(\d+)\n$"
 
-try:
+
+if __name__ == "__main__":
+    total_size = 0
+    status_codes = defaultdict(lambda: 0)
     count = 0
-    for line in sys.stdin:
-        count += 1
-        fields = line.split()[::-1]    # reverse split
-        print(fields)
-        try:
-            acc_size += int(fields[0])
-            code = fields[1]
+    statuses = ("200", "301", "400", "401", "403", "404", "405", "500")
 
-            if code in codes:
-                status_obj[code] = status_obj.get(code, 0) + 1
+    try:
+        for line in sys.stdin:
+            count += 1
+
+            print(line, end='')
+            match = re.fullmatch(exp, line)   # match standard log expression
+            if not match:
+                continue
+
+            code, file_size = match.groups()
+
+            total_size += int(file_size)
+            if code in statuses:
+                status_codes[code] += 1
 
             if count == 10:
-                print_stats(acc_size, status_obj)
+                print_stats(total_size, status_codes)
                 count = 0
-        except Exception:
-            pass
-except KeyboardInterrupt:
-    print_stats(acc_size, status_obj)
-    raise
+    except KeyboardInterrupt as err:
+        print_stats(total_size, status_codes)
+        raise err
