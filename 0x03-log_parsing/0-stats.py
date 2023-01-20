35a4f5e6df7b57a75f1c6c5e6550aa78c461d967
odelolajosh <oladapoodelola@gmail.com>
2023-01-20 07:33:07 +0100

    chore: new impl

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
 """ 0. Log parsing """
 import sys
+import re
+from collections import defaultdict
 
 
-def print_stats(size, obj):
-    """ Prints log stats """
-    print("File size: {}".format(size))
-    for key, val in sorted(obj.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+def print_stats(total_size, status_codes):
+    """ Prints a log stats. """
+    print(f"File size: {total_size}")
+    for status, count in sorted(status_codes.items()):
+        print(f"{status}: {count}")
 
 
-codes = ("200", "301", "400", "401", "403", "404", "405", "500")
-status_obj = {code: 0 for code in codes}
-acc_size = 0
+exp = r"^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}\s-\s\[.*\]\s\".*\"\s(\d{3})\s(\d+)\n$"
 
-try:
-    counter = 0
-    for line in sys.stdin:
-        fields = line.split()[::-1]
-        try:
-            acc_size += int(fields[0])
-            code = fields[1]
 
-            if code in status_obj:
-                status_obj[code] += 1
+if __name__ == "__main__":
+    total_size = 0
+    status_codes: dict = defaultdict(lambda: 0)
+    count = 0
+    statuses = ("200", "301", "400", "401", "403", "404", "405", "500")
 
-            counter += 1
+    try:
+        for line in sys.stdin:
+            count += 1
+            match = re.fullmatch(exp, line)   # match standard log expression
+            if not match:
+                continue
 
-            if counter == 10:
-                print_stats(acc_size, status_obj)
-                counter = 0
-        except Exception:
-            pass
-finally:
-    print_stats(acc_size, status_obj)
+            code, file_size = match.groups()
+
+            total_size += int(file_size)
+            if code in statuses:
+                status_codes[code] += 1
+
+            if count == 10:
+                print_stats(total_size, status_codes)
+                count = 0
+    except KeyboardInterrupt as err:
+        print_stats(total_size, status_codes)
+        raise err
