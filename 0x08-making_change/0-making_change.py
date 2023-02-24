398a3c285b8918c2311bb43804c4fe7240d23c28
odelolajosh <oladapoodelola@gmail.com>
2023-02-24 09:25:33 +0100

    feat: coin change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    """Return a list of coins to make the total.
+
+    Args:
+        coins (list): A list of available coins.
+        total (int): The total amount to make change for.
+
+    Returns:
+        int: the fewest number of coins needed to meet total.
+    """
+    # change count and current coin index
+    change = 0
+    index = 0
+    # sort coins in descending order
+    coins.sort(reverse=True);
+    while total > 0:
+        if index >= len(coins):
+            return -1
+        if coins[index] <= total:
+            total -= coins[index]
+            change += 1
+        else:
+            index += 1
+    return change
