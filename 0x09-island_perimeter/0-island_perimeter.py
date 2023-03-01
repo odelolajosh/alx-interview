f17a3cd32784311fe1205e7156d872a3d3414632
odelolajosh <oladapoodelola@gmail.com>
2023-03-02 00:06:46 +0100

    task: island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" 0. Island Perimeter """
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid """
+    if type(grid) is not list:
+        return 0
+    if not all([type(row) is list for row in grid]):
+        return 0
+    if not all([len(row) == len(grid[0]) for row in grid]):
+        return 0
+
+    perimeter = 0
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            m, n = len(grid) - 1, len(row) - 1
+            neighbors = (
+                i > 0 and grid[i-1][j] == 1,    # top
+                i < m and grid[i+1][j] == 1,    # bottom
+                j > 0 and grid[i][j-1] == 1,    # left
+                j < n and grid[i][j+1] == 1,    # right
+            )
+            # add the contribution of this cell to the perimeter
+            perimeter += 4 - sum(neighbors)
+    return perimeter
